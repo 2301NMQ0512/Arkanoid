759d533bf6f8089d471fbb24bf3489b32ed09bb9
penguingar <vuminh112211@gmail.com>
2025-10-09 13:33:52 +0700

    Update Paddle.java

--- src/collidable/Paddle.java
+++ src/collidable/Paddle.java
@@ -27,3 +27,52 @@ public Paddle(Rectangle, paddle, Color color, biuoop,KeyboardSensor keybboard, i
     step = thespeed;
 }
 
+public void moveLeft() {
+    if (paddle.getUpperLeft().getX() - step <= guiWidthLeft) {
+        this.paddle = new Rectangle(new Point(guiWidthLeft + step + 2,
+                paddle.getUpperLeft().getY()), paddle.getWidth(), paddle.getHeight());
+    }
+    this.paddle = new Rectangle(new Point(paddle.getUpperLeft().getX() - step,
+            paddle.getUpperLeft().getY()), paddle.getWidth(), paddle.getHeight());
+}
+
+public void moveRight() {
+    if (paddle.getUpperLeft().getX() + step + paddle.getWidth() >= guiWidthRight) {
+        this.paddle = new Rectangle(new Point(guiWidthRight - paddle.getWidth() - step + 5,
+                paddle.getUpperLeft().getY()), paddle.getWidth(), paddle.getHeight());
+    }
+    this.paddle = new Rectangle(new Point(paddle.getUpperLeft().getX() + step,
+            paddle.getUpperLeft().getY()), paddle.getWidth(), paddle.getHeight());
+}
+
+public void drawOn(DrawSurface d) {
+    d.setColor(color);
+    d.drawRectangle((int) this.paddle.getUpperLeft().getX(),
+            (int) this.paddle.getUpperLeft().getY(),
+            (int) this.paddle.getWidth(), (int) this.paddle.getHeight());
+    d.fillRectangle((int) paddle.getUpperLeft().getX(),
+            (int) this.paddle.getUpperLeft().getY(),
+            (int) this.paddle.getWidth(), (int) this.paddle.getHeight());
+    d.setColor(Color.black);
+    d.drawRectangle((int) this.paddle.getUpperLeft().getX(),
+            (int) this.paddle.getUpperLeft().getY(),
+            (int) this.paddle.getWidth(), (int) this.paddle.getHeight());
+}
+
+public void timePassed() {
+    if (this.keyboard.isPressed(KeyboardSensor.LEFT_KEY)) {
+        this.moveLeft();
+    }
+    if (this.keyboard.isPressed(KeyboardSensor.RIGHT_KEY)) {
+        this.moveRight();
+    }
+}
+
+public Rectangle getCollisionRectangle() {
+    return this.paddle;
+}
+
+public int checkRegion(Point collisionPoint) {
+    double paddle StartingX = paddle.getUpperleft().getX();
+    
+}
